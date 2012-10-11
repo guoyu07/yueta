97b88ca08fb35c3761a4f5032f6b7cffe8b7e97f
Howe Chiang <howechiang@gmail.com>
2012-10-11 09:09:13 +0800

    项目初始化

--- conf/ioc/dao.js
+++ conf/ioc/dao.js
@@ -6,8 +6,8 @@ var ioc = {
 		},
 		fields : {
 			driverClassName : 'com.mysql.jdbc.Driver',
-			url             : 'jdbc:mysql://218.94.158.227:33041/yueta?useUnicode=true&characterEncoding=UTF-8',
-			//url             : 'jdbc:mysql://192.168.0.44:3306/yueta?useUnicode=true&characterEncoding=UTF-8',
+//			url             : 'jdbc:mysql://218.94.158.227:33041/yueta?useUnicode=true&characterEncoding=UTF-8',
+			url             : 'jdbc:mysql://192.168.0.44:3306/yueta?useUnicode=true&characterEncoding=UTF-8',
 			username        : 'howe',
 			password        : '19860322',
 			initialSize     : 10,
